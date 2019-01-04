495fe44820b3244f6313b9b1407e6fc728871457
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-01-05 01:00:08 +0300

    added routing mechanism and tested with mocha and chai.

--- index.js
+++ index.js
@@ -27,22 +27,67 @@ var server = http.createServer(function (req, res) {
 
     req.on('data', function (data) {
         buffer += decoder.write(data);
-        buffer += decoder.write('h');
     });
 
     req.on('end', function () {
         buffer += decoder.end();
 
-        res.end(`Hello, World!\n${trimmedPath}`);
+        //Choose the handler this request should go to
+        //If one is not found, use 404 as the default
+        var choosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string objects `, queryStringObject);
-        // console.log(`Request received with these headers: \n `, headers);
-        // console.log(`Request received with this payload: `, buffer);
+        // Construct the data object to send to the handler
+        var data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        }
+
+        // Route the request to the handler specified in the router
+        choosenHandler(data, function (statusCode, payload) {
+            // use the status code called back by the handler, or default to 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            // use the payload called by the handler, or default to an empty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            // convert payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            // return the response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log('Returning this response: ', statusCode, payloadString);
+        });
     });
 });
 
 server.listen(port, function () {
     console.log(`The server is listening on ${port} now`);
 });
+
+// Define the handlers
+var handlers = {};
+
+// Sample handler
+handlers.sample = function (data, callback) {
+    // Callback a http status code, and a payload object
+    callback(406, { 'name': 'sample handler', 'payload': data.payload });
+};
+
+handlers.fugazzi = function (data, callback) {
+    callback(407, { 'name:': 'fugazzi handler', 'payload': data.payload })
+}
+
+handlers.notFound = function (data, callback) {
+    callback(404, { 'name': 'not found handler', 'payload': data.payload });
+};
+
+var router = {
+    'sample': handlers.sample,
+    'fugazzi': handlers.fugazzi
+}
 
 module.exports = server;
--- test/test.js
+++ test/test.js
@@ -20,28 +20,89 @@ describe('Array', () => {
     })
 })
 
-describe('index.js', () => {
-    it('http get request should response with path text', () => {
-        chai.request(server)
-            .get('/foo/bar')
-            .end((err, resp) => {
-                resp.should.have.status(200);
-                resp.should.have.property('text', `Hello, World!\nfoo/bar`);
-                console.log(resp.text);
-            })
-        chai.request(server)
-            .get('/foo/bar/fizz/buzz')
-            .end((err, resp) => {
-                resp.should.have.status(200);
-                resp.should.have.property('text', `Hello, World!\nfoo/bar/fizz/buzz`);
-                console.log(resp.text);
-            })
-        chai.request(server)
-            .get('/foo/bar/fizz/buzz/')
-            .end((err, resp) => {
-                resp.should.have.status(200);
-                resp.should.have.property('text', `Hello, World!\nfoo/bar/fizz/buzz`);
-                console.log(resp.text);
-            })
+// describe('index.js', () => {
+//     it('http get request should response with path text', () => {
+//         chai.request(server)
+//             .get('/foo/bar')
+//             .end((err, resp) => {
+//                 resp.should.have.status(200);
+//                 resp.should.have.property('text', `Hello, World!\nfoo/bar`);
+//                 console.log(resp.text);
+//             })
+//         chai.request(server)
+//             .get('/foo/bar/fizz/buzz')
+//             .end((err, resp) => {
+//                 resp.should.have.status(200);
+//                 resp.should.have.property('text', `Hello, World!\nfoo/bar/fizz/buzz`);
+//                 console.log(resp.text);
+//             })
+//         chai.request(server)
+//             .get('/foo/bar/fizz/buzz/')
+//             .end((err, resp) => {
+//                 resp.should.have.status(200);
+//                 resp.should.have.property('text', `Hello, World!\nfoo/bar/fizz/buzz`);
+//                 console.log(resp.text);
+//             })
+//     })
+// })
+
+describe('server', () => {
+    describe('router', () => {
+        it('should handle request with 406 status code, if path match any routes', () => {
+            let payloadObject = {
+                'id': 1,
+                'name': "payloadObject"
+            };
+            chai.request(server)
+                .post('/sample')
+                .send(payloadObject)
+                .end((err, resp) => {
+                    resp.should.have.status(406);
+                    JSON.parse(resp.text).should.a('object');
+                    JSON.parse((JSON.parse(resp.text).payload)).should.a('object');
+                    JSON.parse((JSON.parse(resp.text).payload)).should.have.property('id', 1);
+                    JSON.parse((JSON.parse(resp.text).payload)).should.have.property('name', 'payloadObject');
+                })
+            chai.request(server)
+                .post('/fugazzi')
+                .send(payloadObject)
+                .end((err, resp) => {
+                    resp.should.have.status(407);
+                    JSON.parse(resp.text).should.a('object');
+                    JSON.parse((JSON.parse(resp.text).payload)).should.a('object');
+                    JSON.parse((JSON.parse(resp.text).payload)).should.have.property('id', 1);
+                    JSON.parse((JSON.parse(resp.text).payload)).should.have.property('name', 'payloadObject');
+                })
+        })
+        it('should handle request with 404 status code, if path does not match any routes', () => {
+            let payloadObject = {
+                'id': 1,
+                'name': "payloadObject"
+            };
+            chai.request(server)
+                .post('/sample1')
+                .send(payloadObject)
+                .end((err, resp) => {
+                    resp.should.have.status(404);
+                    JSON.parse(resp.text).should.a('object');
+                    JSON.parse(resp.text).should.have.property('name', 'not found handler');
+                })
+            chai.request(server)
+                .post('/fizz/buzz')
+                .send(payloadObject)
+                .end((err, resp) => {
+                    resp.should.have.status(404);
+                    JSON.parse(resp.text).should.a('object');
+                    JSON.parse(resp.text).should.have.property('name', 'not found handler');
+                })
+            chai.request(server)
+                .post('/foo/bar/')
+                .send(payloadObject)
+                .end((err, resp) => {
+                    resp.should.have.status(404);
+                    JSON.parse(resp.text).should.a('object');
+                    JSON.parse(resp.text).should.have.property('name', 'not found handler');
+                })
+        })
     })
 })
